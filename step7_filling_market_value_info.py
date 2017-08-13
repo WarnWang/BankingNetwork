002f67d0c1df5ddf210d0e4a2d8b357b74701633
Mark Wang <wangyouan0629@hotmail.com>
2017-08-13 15:27:36 +0800

    fill in missing market value

--- step7_filling_market_value_info.py
+++ step7_filling_market_value_info.py
@@ -32,21 +32,23 @@ def fill_in_market_value(row):
 
         if os.path.isfile(os.path.join(const.CUSIP_MARKET_VALUE_PATH, '{}10'.format(cusip))):
             mve = pd.read_pickle(os.path.join(const.CUSIP_MARKET_VALUE_PATH, '{}10'.format(cusip)))
-            if ann_date in mve.index:
-                return mve[ann_date]
+            mve = mve[mve.index <= ann_date]
+            if not mve.empty:
+                return mve.iloc[-1]
 
     if hasattr(ticker, 'upper'):
         ticker = ticker.upper()
         if os.path.isfile(os.path.join(const.TICKER_MARKET_VALUE_PATH, ticker)):
             mve = pd.read_pickle(os.path.join(const.TICKER_MARKET_VALUE_PATH, ticker))
-            if ann_date in mve.index:
-                return mve[ann_date]
+            mve = mve[mve.index <= ann_date]
+            if not mve.empty:
+                return mve.iloc[-1]
 
     return np.nan
 
 
 if __name__ == '__main__':
-    run_up_df = pd.read_pickle(os.path.join(const.TEMP_PATH, '20180812_SDC_MnA_add_runup_1986_2016.p'))
+    run_up_df = pd.read_pickle(os.path.join(const.TEMP_PATH, '20170813_SDC_MnA_fill_in_LT.p'))
     run_up_df[const.ACQUIRER_MVE] = run_up_df.apply(fill_in_market_value, axis=1)
     run_up_df.to_pickle(os.path.join(const.TEMP_PATH, '20170813_SDC_MnA_fill_in_MVE.p'))
     run_up_df.to_csv(os.path.join(const.RESULT_PATH, '20170813_SDC_MnA_fill_in_MVE.csv'), index=False)
