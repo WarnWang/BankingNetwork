882b23a288f3631863a694167eff7c5bca4a5f12
Mark Wang <wangyouan0629@hotmail.com>
2017-08-31 12:55:39 +0800

    Finish step2

---
+++ generate_psm/step2_sort_input_files.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: step2_try_to_do_pscore
+# @Date: 31/8/2017
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
+
+import os
+
+import pandas as pd
+
+from constants import Constants as const
+
+df = pd.read_excel(os.path.join(const.DATA_PATH, '20170829_CAR_Control_Ind_IV12_DirExe.xlsx'))
+
+useful_column = [const.ANNOUNCED_DATE]
+
+for tag in [const.ACQUIRER, const.TARGET]:
+    df.loc[:, '{}_{}'.format(tag, const.REAL)] = 1
+    useful_column.append('{}_{}'.format(tag, const.REAL))
+
+df.to_pickle(os.path.join(const.TEMP_PATH, '20170831_CAR_real_tag.pkl'))
+
+useful_df = df.dropna(subset=['{}_{}'.format(const.ACQUIRER, const.LINK_TABLE_RSSD9001),
+                              '{}_{}'.format(const.TARGET, const.LINK_TABLE_RSSD9001)], how='any')
+
+for tag in [const.ACQUIRER, const.TARGET]:
+    key = '{}_{}'.format(tag, const.LINK_TABLE_RSSD9001)
+    useful_df[key] = pd.to_numeric(useful_df[key], errors='raise', downcast='integer')
+    useful_column.append(key)
+
+useful_df.to_pickle(os.path.join(const.TEMP_PATH, '20170831_CAR_useful_row.pkl'))
+
+useful_df = useful_df[useful_column]
+
+useful_df.to_pickle(os.path.join(const.TEMP_PATH, '20170831_CAR_useful_col.pkl'))
