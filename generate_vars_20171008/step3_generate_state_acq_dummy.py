7055f605b8617409a51fcc84ecd96951ecddc90b
Mark Wang <wangyouan0629@hotmail.com>
2017-10-08 15:43:08 +0800

    add new file

---
+++ generate_vars_20171008/step3_generate_state_acq_dummy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: step3_generate_state_acq_dummy
+# @Date: 8/10/2017
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
+
+import os
+
+import pandas as pd
+
+from constants import Constants as const
+
+df = pd.read_pickle(os.path.join(const.TEMP_PATH, '20171008_state_state_data_add_distance.pkl'))
+acq_df = pd.read_pickle(os.path.join(const.DATA_PATH, '20171008_SDC_bank_merger_state.xlsx'))
+
+acq_df = acq_df[[const.YEAR, 'Acquirer_State_abbr', 'Target_State_abbr']]
+
+acq_df = acq_df.rename(index=str, columns={"Acquirer_State_abbr": 'acq',
+                                           'Target_State_abbr': 'tgt',
+                                           const.YEAR: 'year'})
+
+acq_df.loc[:, 'acq_state_acquiring_tar_state_dummy'] = 1
+
+merged_df = pd.merge(df, acq_df, on=['year', 'acq', 'tgt'], how='left')
+merged_df['acq_state_acquiring_tar_state_dummy'] = merged_df['acq_state_acquiring_tar_state_dummy'].fillna(0)
+
+merged_df.to_pickle(os.path.join(const.TEMP_PATH, '20171008_state_state_data_add_dep_var.pkl'))
+merged_df.to_stata(os.path.join(const.RESULT_PATH, '20171008_state_state_data_add_dep_var.dta'))
