ffb697d4e0173a7f117d0104874027a102803ef1
Mark Wang <wangyouan0629@hotmail.com>
2017-10-08 16:15:21 +0800

    fix an error in file3

--- generate_vars_20171008/step3_generate_state_acq_dummy.py
+++ generate_vars_20171008/step3_generate_state_acq_dummy.py
@@ -13,7 +13,9 @@ import pandas as pd
 from constants import Constants as const
 
 df = pd.read_pickle(os.path.join(const.TEMP_PATH, '20171008_state_state_data_add_distance.pkl'))
-acq_df = pd.read_pickle(os.path.join(const.DATA_PATH, '20171008_SDC_bank_merger_state.xlsx'))
+df['year'] = df.year.apply(int)
+
+acq_df = pd.read_excel(os.path.join(const.DATA_PATH, '20171008_SDC_bank_merger_state.xlsx'))
 
 acq_df = acq_df[[const.YEAR, 'Acquirer_State_abbr', 'Target_State_abbr']]
 
@@ -24,7 +26,9 @@ acq_df = acq_df.rename(index=str, columns={"Acquirer_State_abbr": 'acq',
 acq_df.loc[:, 'acq_state_acquiring_tar_state_dummy'] = 1
 
 merged_df = pd.merge(df, acq_df, on=['year', 'acq', 'tgt'], how='left')
-merged_df['acq_state_acquiring_tar_state_dummy'] = merged_df['acq_state_acquiring_tar_state_dummy'].fillna(0)
+merged_df['acq_state_acquiring_tar_state_dummy'] = merged_df['acq_state_acquiring_tar_state_dummy'].fillna(0).apply(int)
+merged_df['acq_access_tgt'] = merged_df['acq_access_tgt'].apply(int)
+merged_df['tgt_access_acq'] = merged_df['tgt_access_acq'].apply(int)
 
 merged_df.to_pickle(os.path.join(const.TEMP_PATH, '20171008_state_state_data_add_dep_var.pkl'))
 merged_df.to_stata(os.path.join(const.RESULT_PATH, '20171008_state_state_data_add_dep_var.dta'))
