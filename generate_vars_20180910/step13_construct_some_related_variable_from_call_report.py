d08335ca08e751226ee2d8484e84f4bcd6653e35
Mark <wangyouan0629@hotmail.com>
2018-10-04 11:16:37 +0800

    generate_vars_20180910\step12_append_some_needed_variables.py * fix a typo

--- generate_vars_20180910/step13_construct_some_related_variable_from_call_report.py
+++ generate_vars_20180910/step13_construct_some_related_variable_from_call_report.py
@@ -25,6 +25,7 @@ NON_RESIDENTiAL_PROP = 'RCON1480'
 TOTAL_ASSETS = 'RCFD2170'
 MORTGAGE_BACK_SEC = 'RCFD8639'
 COM_IND_LOAN = 'RCFD1766'
+COM_IND_LOAN = 'RCFD1766'
 CONSUMER_LOAN = 'RCFD1975'
 INTEREST_FROM_CI_LOAN = 'RIAD4012'
 INTEREST_FROM_RE_LOAN = 'RIAD4011'
@@ -35,6 +36,17 @@ MORTGAGE_BACK_SEC_SUB2 = 'RCFD0602'
 USEFUL_KEYS = [const.COMMERCIAL_RSSD9001, const.COMMERCIAL_RSSD9364, const.YEAR, REAL_ESTATE_LOAN, NON_RESIDENTiAL_PROP,
                TOTAL_ASSETS, MORTGAGE_BACK_SEC, COM_IND_LOAN, CONSUMER_LOAN, INTEREST_FROM_CI_LOAN,
                INTEREST_FROM_RE_LOAN, MORTGAGE_BACK_SEC_SUB, MORTGAGE_BACK_SEC_SUB2]
+
+
+def generate_2015_2016_data(tmp_df):
+    result_df = tmp_df.copy()
+    for year in [2015, 2016]:
+        tmp_sub_df = tmp_df[tmp_df[const.YEAR] == 2014].copy()
+        tmp_sub_df.loc[:, const.YEAR] = year
+        result_df = result_df.append(tmp_sub_df, ignore_index=True, sort=False)
+
+    return result_df
+
 
 if __name__ == '__main__':
     data_df = pd.read_pickle(os.path.join(const.TEMP_PATH, '20181003_third_part_concise_3018_add_some_vars.pkl'))
@@ -51,3 +63,28 @@ if __name__ == '__main__':
         call_dfs.append(call_df[useful_keys].copy())
 
     merged_call_df = pd.concat(call_dfs, ignore_index=True, sort=False)
+    merged_call_df.loc[:, MORTGAGE_BACK_SEC] = merged_call_df[MORTGAGE_BACK_SEC].fillna(
+        merged_call_df[MORTGAGE_BACK_SEC_SUB2] + merged_call_df[MORTGAGE_BACK_SEC_SUB]
+    )
+    for key in [const.COMMERCIAL_RSSD9364, const.COMMERCIAL_RSSD9001]:
+        merged_call_df.loc[:, key] = merged_call_df[key].dropna().apply(lambda x: str(int(x)))
+    merged_call_df_9001 = merged_call_df.drop([const.COMMERCIAL_RSSD9364], axis=1).dropna(
+        subset=[const.COMMERCIAL_RSSD9001]).groupby([const.COMMERCIAL_RSSD9001, const.YEAR]).sum().reset_index(
+        drop=False)
+    merged_call_df_9364 = merged_call_df.drop([const.COMMERCIAL_RSSD9001], axis=1).dropna(
+        subset=[const.COMMERCIAL_RSSD9364]).groupby([const.COMMERCIAL_RSSD9364, const.YEAR]).sum().reset_index(
+        drop=False).rename(index=str, columns={const.COMMERCIAL_RSSD9364: const.COMMERCIAL_RSSD9001})
+    merged_call_df_9364 = merged_call_df_9364[merged_call_df_9364[const.COMMERCIAL_RSSD9001] != '0']
+    cdf = merged_call_df_9001.append(merged_call_df_9364, ignore_index=True, sort=False)
+    cdf = cdf.drop_duplicates(subset=[const.COMMERCIAL_RSSD9001, const.YEAR], keep='last')
+    cdf = generate_2015_2016_data(cdf)
+
+    cdf.loc[:, const.REAL_ESTATE_LOANS] = (cdf[REAL_ESTATE_LOAN] - cdf[NON_RESIDENTiAL_PROP]) / cdf[TOTAL_ASSETS]
+    cdf.loc[:, const.MBS] = cdf[MORTGAGE_BACK_SEC] / cdf[TOTAL_ASSETS]
+    cdf.loc[:, const.COMMERCIAL_MORTGAGES] = cdf[NON_RESIDENTiAL_PROP] / cdf[TOTAL_ASSETS]
+    cdf.loc[:, const.C_AND_I_LOANS] = cdf[COM_IND_LOAN] / cdf[TOTAL_ASSETS]
+    cdf.loc[:, const.CONSUMER_LOANS] = cdf[CONSUMER_LOAN] / cdf[TOTAL_ASSETS]
+    cdf.loc[:, const.C_AND_I_LOAN_PROFITABILITY] = cdf[INTEREST_FROM_CI_LOAN] / cdf[COM_IND_LOAN]
+    cdf.loc[:, const.REAL_ESTATE_LOAN_PROFITABILITY] = cdf[INTEREST_FROM_RE_LOAN] / cdf[REAL_ESTATE_LOAN]
+    cdf.loc[:, const.PROFITABILITY_RATIO] = cdf[const.C_AND_I_LOAN_PROFITABILITY] / cdf[
+        const.REAL_ESTATE_LOAN_PROFITABILITY]
