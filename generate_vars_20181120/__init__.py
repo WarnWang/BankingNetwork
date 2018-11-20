a3016ea32f5b03e2c2b0798e1356103c663bb878
Mark Wang <wangyouan0629@hotmail.com>
2018-11-20 14:20:24 +0800

    generate_vars_20181120 (add) Create new path

---
+++ generate_vars_20181120/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: __init__.py
+# @Date: 20/11/2018
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
+
+"""
+This project aims to finish two task
+
+1. From rate watch. Dep variable is branch small business lending and pricing, independent variable is branch's county
+experiences number of A + T merger
+
+2. County-year observation:
+    (1) number and percent of exit of other bank's branches in one county (exclude all merger and acquirer).
+    (2) number and percent of entry of other bank's branches in one county (exclude all merger and acquirer).
+    (3) number of other bank's total branches in current year, next year and last year.
+    (4) county HHI, growth, last year HHI
+    (5) county total deposit (other banks), growth.
+"""
