2d42fc3306d12477614e63bfb5bed26948a9194a
Mark Wang <wangyouan0629@hotmail.com>
2018-11-28 11:06:24 +0800

    generate_vars_20181120\step10_generate_bhc_county_level.py * fix some constant error + drop some invalid observations

--- generate_vars_20181120/step10_generate_bhc_county_level.py
+++ generate_vars_20181120/step10_generate_bhc_county_level.py
@@ -65,7 +65,10 @@ if __name__ == '__main__':
     bank_branch_df: DataFrame = pd.read_pickle(
         os.path.join(const.TEMP_PATH, '20181120_branch_location_total_deposit_info.pkl'))
 
+    bank_branch_df = bank_branch_df[bank_branch_df[const.RSSD9001] != 0].copy()
+
     county_branch_df: DataFrame = bank_branch_df.groupby(const.RSSD9001).apply(calculate_bank_county_info).reset_index(
         drop=False)[[const.RSSD9001, const.FIPS, const.YEAR, const.EXIT_BRANCH_NUM, const.ENTRY_BRANCH_NUM,
-                     const.BRANCH_NUM, const.TOTAL_DEPOSITS]].copy()
+                     const.BRANCH_NUM, const.TOTAL_DEPOSITS_REAL]].copy()
+    county_branch_df = county_branch_df[county_branch_df[const.RSSD9001] != 0]
     county_branch_df.to_pickle(os.path.join(const.TEMP_PATH, '20181127_county_branch_status_count.pkl'))
